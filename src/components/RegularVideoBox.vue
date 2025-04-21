37a6883e09fb4fb9c536df014459c7b559fb6c23
Nikola Cirkovic <cirkovic.developer@gmail.com>
2025-04-21 18:27:50 +0200

    Regular video box component for video wall

---
+++ src/components/RegularVideoBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="shadow-lg">
+        <img class="object-contain hover:cursor-pointer hover:opacity-70"
+            src="https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives-768x492.png" alt="">
+        <p class="text-center text-black text-2xl break-words">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Perspiciatis nisi, provident ipsa ducimus aliquam quae.</p>
+        <div class="flex justify-between m-2">
+            <p> <i class="fa-solid fa-calendar"></i> Pre 2 sata</p>
+            <p> <i class="fa-solid fa-comment"></i> : 2</p>
+        </div>
+    </div>
+</template>
