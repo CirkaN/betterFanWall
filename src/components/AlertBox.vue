7b6bc31c0aa6eead870531f9412888057793d985
Nikola Cirkovic <cirkovic.developer@gmail.com>
2025-04-21 18:27:33 +0200

    AlertBox wip component

---
+++ src/components/AlertBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-5 flex justify-center">
+        <p class="text-xl ">{{ props.alertText }}</p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["alertText", "alertType"]);
+</script>
