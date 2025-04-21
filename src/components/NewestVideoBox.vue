867d11bd80761a016612c1a3e72bd912a81afbc2
Nikola Cirkovic <cirkovic.developer@gmail.com>
2025-04-21 18:28:01 +0200

    Newest video box wip

---
+++ src/components/NewestVideoBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-1/3 flex flex-col">
+        <div class="h-1/3">
+            <div class="relative w-[900px]">
+                <img class="object-contain hover:cursor-pointer hover:opacity-70 w-full"
+                    src="https://assets.justinmind.com/wp-content/uploads/2018/11/Lorem-Ipsum-alternatives-768x492.png"
+                    alt="">
+                <div class="absolute bottom-0 left-0 right-0 px-4 py-2 bg-gray-800 opacity-70">
+                    <h3 class="text-3xl text-white font-bold">
+                        Video example title 1 </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
